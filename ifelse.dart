f7b58c90031741831fcfd908b1bbf2e28e0278a7
azrialdi84 <azrialdi84@gmail.com>
2024-01-27 17:19:06 +0700

    belajar swithch

--- ifelse.dart
+++ ifelse.dart
@@ -1,12 +1,23 @@
 void main() {
   
 var nilai = 70;
-var absen = 90;
+var absen = 60;
 
 if(nilai >= 75 && absen >= 75){
   print('anda lulus');
 } else {
   print('anda tidak lulus');
 }
+
+if(nilai >= 75 && absen >= 75){
+  print('A');
+} else if (nilai >= 70 && absen >= 70){
+  print('B');
+}else if (nilai >= 60 && absen >= 60){
+  print('C');
+}else if (nilai >= 50 && absen >= 50){
+  print('D');
+}
+
 
 }
---
+++ switch.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  
+  var nilai ='D';
+
+  switch (nilai) {
+    case 'A' :
+      print('selamat anda lulus mumtaz');
+      break;
+    case 'B':
+    case 'C':
+      print('anda Lulus');
+      break;
+
+    default:
+    print('Anda kurang beruntumg');
+  }
+}
