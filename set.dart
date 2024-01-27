c273b803b054159a6acae248b7f9acd09e9cb2d6
azrialdi84 <azrialdi84@gmail.com>
2024-01-27 16:53:21 +0700

    belajar set

---
+++ set.dart
@@ -0,0 +1,28 @@
+void main() {
+  Set<int> numbers = {};
+  var doubles = <double>{};
+
+  print(numbers);
+
+   var names = <String>{
+    'eko',
+    'eko',
+    'kurniawan',
+    'toni'
+
+   };
+
+   //names.add('eko');
+  // names.add('eko');
+   //names.add('kurniawan');
+   //names.add('jono');
+
+   print(names);
+   print(names.length);
+
+   names.remove('eko');
+   print(names);
+   
+
+
+}
